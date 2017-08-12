e4ac8490cbc7a4ef8a9112122beb6f48c4569601
Rejina15 <30853145+Rejina15@users.noreply.github.com>
2017-08-12 10:28:01 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#inclide<conio.h>
+int main()
+{
+int num;
+printf("  \nEnter the number");
+scanf("  %d"  ,&num);
+if(num%2==0)
+{
+printf(""   \nEven"  );
+}
+else
+{
+printf("   \nOdd"  );
+}
+getch();
+return 0;
+}
